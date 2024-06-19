5d398384c5fe454ffbec1420ab01d1ec1a2bb9a4
USER <USER@1.209.180.138>
2024-06-19 12:12:21 +0900

    <2024.06.19> - git 복습용 최초 커밋

---
+++ java_git_test/src/java_git_test/javaEx.java
@@ -0,0 +1,10 @@
+package java_git_test;
+
+public class javaEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_git_test/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module java_git_test {
+}
